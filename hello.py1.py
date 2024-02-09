e804ee8c6a931a1fae9ed978e89c6588c2ea3770
MOHAN <mohangubbala93@gmail.com>
2024-02-09 23:20:22 +0530

    added hello.py1.py

---
+++ hello.py1.py
@@ -0,0 +1,9 @@
+def main():
+    # Prompt the user to enter their name
+    name = input("Enter your name: ")
+    
+    # Greet the user
+    print("Hello, " + name + "! Welcome to our program.")
+
+if __name__ == "__main__":
+    main()
